f42e42261cc009ab77d76805e1f977f766bc9cb3
rafia37 <rafia0037@gmail.com>
2018-09-13 16:14:52 -0700

    Modifying pipeline to accomodate individual AORs

---
+++ sep_aor_flux_gen_p1.py
@@ -0,0 +1,25 @@
+import numpy as np
+import functions as func
+import pdb, glob
+from tqdm import tqdm
+from astropy.table import Table
+from astropy.io import fits, ascii
+from astropy.wcs import WCS
+from astropy import units as u
+from astropy.coordinates import SkyCoord
+
+
+AORnames = np.sort(glob.glob('/data1/phot_cal/spitzer/subsets/hd165459/r*/'))
+AORs = []
+
+for aor in AORnames:
+    fnames = np.sort(glob.glob(aor + 'ch1/bcd/*_bcd.fits'))
+    AORs.append(fnames)
+    
+
+#Provide proper sky coordinates in hms
+# '18 02 30.7410086899 +58 37 38.157415821' for HD 165459
+# '17 24 52.2772360943 +60 25 50.780790994' for BD +60 1753
+
+sky = SkyCoord('18 02 30.7410086899 +58 37 38.157415821', unit=(u.hourangle, u.deg))
+
