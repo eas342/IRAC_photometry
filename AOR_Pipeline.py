a6433badefe6b71cf883f8219178b14c38abef82
rafia37 <rafia0037@gmail.com>
2018-10-10 11:45:40 -0700

    changed pipeline to auto-determine mission

--- AOR_Pipeline.py
+++ AOR_Pipeline.py
@@ -14,7 +14,7 @@ from datetime import datetime
 
 #Creating a table generating function that can be called from scripts
 #---------------------------------------------------------------------
-def run(AORs, sky, r, rIn, rOut, ap_corr, pixArea, mission, channel, N):
+def run(AORs, sky, r, rIn, rOut, ap_corr, pixArea, channel, N):
     #initializing table to hold results
     result = Table(names = ('AORKEY', 'DateObs', 'Cycling DPattern', 'DScale', 'DPosition', 'FTime (sec)', 'Time (MJD)', 'Flux (mJy)', 'Error (mJy)', 'Spread (%)', 'Outliers Rejected'), dtype = ('i4', 'S25', 'S5', 'S10', 'S5', 'f8', 'f8', 'f8', 'f8', 'f8', 'i4'))
 
@@ -41,11 +41,12 @@ def run(AORs, sky, r, rIn, rOut, ap_corr, pixArea, mission, channel, N):
 
         #Defining every table element except for flux terms
         #..................................................
-        aKey = header['AORKEY']
-        dObs = header['DATE_OBS']
-        DPat = 'YES' if 'cycl'in header['AORLABEL'] else 'NO'
-        fTim = header['FRAMTIME']
-        time = header['MJD_OBS']
+        aKey    = header['AORKEY']
+        dObs    = header['DATE_OBS']
+        DPat    = 'YES' if 'cycl'in header['AORLABEL'] else 'NO'
+        fTim    = header['FRAMTIME']
+        time    = header['MJD_OBS']
+        mission = 'Cryogenic' if (time<=54968) else 'Warm'
         try:
             DScl = header['DITHSCAL']
         except:
@@ -119,7 +120,7 @@ if __name__=='__main__':
     #Defining general constants
     #---------------------------
 
-    print '\n', 'Please input the following arguments and seperate them with a semi-colon(;)-- \n', '1. File Path up to aor names (e.g. /data1/phot_cal/spitzer/hd165459/cryo/r*/) \n', '2. File type (eg. bcd) \n', '3. Target Name (e.g. HD 165459)\n', '4. Target Coordinates (e.g. 18 02 30.7410086899 +58 37 38.157415821) \n', '5. Mission (e.g. Cryogenic or Warm)\n','6. Source Aperture Radius in px (eg. 10) \n', '7. Inner Background Radius in px (eg. 12) \n', '8. Outer Background Radius in px (eg. 20) \n', '9. Channel# (1,2,3 or 4) \n', '10. Aperture Correction Factor (collect from iracinstrumenthandbook/27, e.g. 1.000) \n', '11. Length of a pixel in arcsec (e.g. 1.221 for ch1) \n', '12. Run# (For output file name. Should be an integer)\n', '13. Sigma Clipping Number (e.g. 10) \n', '14. Comments (to be included in the log) \n', 'Don\'t jump to any argument. e.g. You can\'t skip sigma to get to comments. comments must be the 14th argument. \n', 'You could however, use \'n/a\' for sigma if you don\'t want sigma clipping \n', 'Example command: /data1/phot_cal/spitzer/hd165459/cryo/r*/;bcd;HD 165459;18 02 30.7410086899 +58 37 38.157415821;Cryogenic;10;12;20;1;1.0;1.221;5;10;Your comment goes here. \n', '\n'
+    print '\n', 'Please input the following arguments and seperate them with a semi-colon(;)-- \n', '1. File Path up to aor names (e.g. /data1/phot_cal/spitzer/hd165459/cryo/r*/) \n', '2. File type (eg. bcd) \n', '3. Target Name (e.g. HD 165459)\n', '4. Target Coordinates (e.g. 18 02 30.7410086899 +58 37 38.157415821) \n', '5. Readout Mode (e.g. full or sub)\n','6. Source Aperture Radius in px (eg. 10) \n', '7. Inner Background Radius in px (eg. 12) \n', '8. Outer Background Radius in px (eg. 20) \n', '9. Channel# (1,2,3 or 4) \n', '10. Aperture Correction Factor (collect from iracinstrumenthandbook/27, e.g. 1.000) \n', '11. Length of a pixel in arcsec (e.g. 1.221 for ch1) \n', '12. Run# (For output file name. Should be an integer)\n', '13. Sigma Clipping Number (e.g. 10) \n', '14. Comments (to be included in the log) \n', 'Don\'t jump to any argument. e.g. You can\'t skip sigma to get to comments. comments must be the 14th argument. \n', 'You could however, use \'n/a\' for sigma if you don\'t want sigma clipping \n', 'Example command: /data1/phot_cal/spitzer/hd165459/cryo/r*/;bcd;HD 165459;18 02 30.7410086899 +58 37 38.157415821;full;10;12;20;1;1.0;1.221;5;10;Your comment goes here. \n', '\n'
 
     const_str = raw_input('Input the parameters listed above: ') 
     constants = const_str.split(';')
@@ -141,8 +142,6 @@ if __name__=='__main__':
     # '18 02 30.7410086899 +58 37 38.157415821' for HD 165459
     # '17 24 52.2772360943 +60 25 50.780790994' for BD +60 1753
     sky = SkyCoord(constants[3], unit=(u.hourangle, u.deg))
-
-    mission = constants[4]
     
     r, rIn, rOut = int(constants[5]), int(constants[6]), int(constants[7])
     
@@ -163,7 +162,7 @@ if __name__=='__main__':
     
     
     #Generating & writing data tables to csv files
-    res, data, prob = run(AORs, sky, r, rIn, rOut, ap_corr, pixArea, mission, channel, N)
+    res, data, prob = run(AORs, sky, r, rIn, rOut, ap_corr, pixArea, channel, N)
     ascii.write(res, 'Reduction_Data_&_Logs/run%s_aor_data.csv' % constants[11], delimiter = ',', overwrite = True)
     ascii.write(data, 'Reduction_Data_&_Logs/run%s_img_data.csv' % constants[11], delimiter = ',', overwrite = True) 
 
@@ -176,9 +175,8 @@ if __name__=='__main__':
     log.write("Command Used : %s \n" % const_str)
     log.write("Instrument   : IRAC Channel %s \n" % constants[8])
     log.write("File Type    : %s \n" % constants[1].upper())
-    log.write("Mission      : %s \n" % constants[4])
     log.write("Target       : %s \n" % constants[2])
-    log.write("Radius Used  : src_r: %i, bkg_in: %i, bkg_out: %i \n" % (r, rIn, rOut))
+    log.write("Radius Used  : src_r-%i, bkg_in-%i, bkg_out-%i \n" % (r, rIn, rOut))
     log.write("Average Flux : %.2f +- %.2f \n" % (np.mean(res['Flux (mJy)']), np.std(res['Flux (mJy)'])))
     log.write("Problem AORs : " + ("%i "*len(prob) % tuple(prob)) + "\n")
     log.write("Comments     : %s" % constants[13])
