60daf65afda877572784b5ec3b8bd453563cc1e9
rafia37 <rafia0037@gmail.com>
2018-10-18 15:11:29 -0700

    Tested the new pipeline structure. All good. Ready to run on the new datasets

--- AOR_Pipeline.py
+++ AOR_Pipeline.py
@@ -16,7 +16,7 @@ from datetime import datetime
 #---------------------------------------------------------------------
 def run(crdFormat, aor_crd, channel, filetype, r, rIn, rOut, ap_corr, pixLen, N):
     #initializing table to hold results
-    result = Table(names = ('AORKEY', 'DateObs', 'Mission', 'Read Mode', 'Cycling DPattern', 'DScale', 'DPosition', 'FTime (sec)', 'Time (MJD)', 'Flux (mJy)', 'Error (mJy)', 'Spread (%)', 'Outliers Rejected'), dtype = ('i4', 'S25', 'S5', 'S5', 'S5', 'S10', 'S5', 'f8', 'f8', 'f8', 'f8', 'f8', 'i4'))
+    result = Table(names = ('AORKEY', 'Target Coordinates', 'DateObs', 'Mission', 'Read Mode', 'Cycling DPattern', 'DScale', 'DPosition', 'FTime (sec)', 'Time (MJD)', 'Flux (mJy)', 'Error (mJy)', 'Spread (%)', 'Outliers Rejected'), dtype = ('i4', 'S25', 'S25', 'S5', 'S5', 'S5', 'S10', 'S5', 'f8', 'f8', 'f8', 'f8', 'f8', 'i4'))
 
 
     problem = []
@@ -35,13 +35,15 @@ def run(crdFormat, aor_crd, channel, filetype, r, rIn, rOut, ap_corr, pixLen, N)
         
     elif crdFormat.lower() == 'multiple hms':
         skyTable = ascii.read(aor_crd)
-        skyList = [SkyCoord(st, unit=(u.hourangle, u.deg)) for st in skyTable['Target Coordinates']]
-        aorList = [glob.glob(aor + '/ch%i/bcd/*_%s.fits' % (channel, filetype)) for aor in skyTable['AOR Filepath']]
+        AORs     = skyTable['AOR Filepath']
+        skyList  = [SkyCoord(st, unit=(u.hourangle, u.deg)) for st in skyTable['Target Coordinates']]
+        aorList  = [glob.glob(aor + '/ch%i/bcd/*_%s.fits' % (channel, filetype)) for aor in AORs]
         
     elif crdFormat.lower() == 'multiple deg':
         skyTable = ascii.read(aor_crd)
-        skyList = [SkyCoord(st, unit=u.deg) for st in skyTable['Target Coordinates']]
-        aorList = [glob.glob(aor + '/ch%i/bcd/*_%s.fits' % (channel, filetype)) for aor in skyTable['AOR Filepath']]
+        AORs     = skyTable['AOR Filepath']
+        skyList  = [SkyCoord(st, unit=u.deg) for st in skyTable['Target Coordinates']]
+        aorList  = [glob.glob(aor + '/ch%i/bcd/*_%s.fits' % (channel, filetype)) for aor in AORs]
         
     else:
         print('Please input one of these 4 values for crdFormat: single hms, single deg, multiple hms, multiple deg')
@@ -53,14 +55,18 @@ def run(crdFormat, aor_crd, channel, filetype, r, rIn, rOut, ap_corr, pixLen, N)
     for i, (aor, sky) in tqdm(enumerate(zip(aorList, skyList))):
         
         #Performing photometry on every image in an AOR
+        counter = 0
         if len(aor)>0:
             data, header = func.single_target_phot(aor, sky, r, rIn, rOut)
+            counter += 1
         else:
-            problem.append(header['AORKEY'])
+            problem.append(AORs[i].split('/r')[1])
+            print('Empty AOR encountered: %s' % AORs[i].split('/r')[1])
+            continue
 
 
         #Cumulating all the data tables from every aor
-        if i==0:
+        if counter==1:
             cum_data = data
         else:
             cum_data = vstack([cum_data, data])
@@ -75,6 +81,7 @@ def run(crdFormat, aor_crd, channel, filetype, r, rIn, rOut, ap_corr, pixLen, N)
         time = header['MJD_OBS']
         mssn = 'CRYO' if (time<=54968) else 'WARM'
         mode = header['READMODE']
+        tCrd = str(sky).replace(')','(').split('(')[-2] 
         try:
             dScl = header['DITHSCAL']
         except:
@@ -143,7 +150,7 @@ def run(crdFormat, aor_crd, channel, filetype, r, rIn, rOut, ap_corr, pixLen, N)
         #........................
 
         
-        result.add_row([aKey, dObs, mssn, mode, dPat, dScl, dPos, fTim, time, flux, error, spread, clipped])
+        result.add_row([aKey, tCrd, dObs, mssn, mode, dPat, dScl, dPos, fTim, time, flux, error, spread, clipped])
     
     return result, cum_data, problem
 
@@ -156,14 +163,14 @@ if __name__=='__main__':
     #Defining general constants
     #---------------------------
 
-    print '\n', 'Please input the following arguments and seperate them with a semi-colon(;)-- \n', '1. Coordinate format (e.g. single hms, single deg, multiple hms, multiple deg) \n', '2. AOR file path and coordinates (e.g. /data1/phot_cal/spitzer/hd165459/cryo/r*,18 02 30.7410086899 +58 37 38.157415821 \n', '3. File type (eg. bcd, cbcd, sub2d) \n', '4. Target Name (e.g. HD 165459, BD + 60 1753) \n','5. Source Aperture Radius in px (eg. 10) \n', '6. Inner Background Radius in px (eg. 12) \n', '7. Outer Background Radius in px (eg. 20) \n', '8. Channel# (1,2,3 or 4) \n', '9. Aperture Correction Factor (collect from iracinstrumenthandbook/27, e.g. 1.000) \n', '10. Length of a pixel in arcsec (e.g. 1.221 for ch1) \n', '11. Run# (For output file name. Should be an integer)\n', '12. Sigma Clipping Number (e.g. 10) \n', '13. Comments (to be included in the log) \n', 'Don\'t jump to any argument. e.g. You can\'t skip sigma to get to comments. comments must be the 14th argument. \n', 'You could however, use \'n/a\' for sigma if you don\'t want sigma clipping \n', 'Example command: /data1/phot_cal/spitzer/hd165459/cryo/r*/;bcd;HD 165459;18 02 30.7410086899 +58 37 38.157415821;10;12;20;1;1.0;1.221;5;10;Your comment goes here. \n', '\n'
+    print '\n', 'Please input the following arguments and seperate them with a semi-colon(;)-- \n', '1. Coordinate format (e.g. single hms, single deg, multiple hms, multiple deg) \n', '2. AOR file path and coordinates (e.g. /data1/phot_cal/spitzer/hd165459/cryo/r*,18 02 30.7410086899 +58 37 38.157415821 \n', '3. File type (eg. bcd, cbcd, sub2d) \n', '4. Target Name (e.g. HD 165459, BD + 60 1753) \n','5. Source Aperture Radius in px (eg. 10) \n', '6. Inner Background Radius in px (eg. 12) \n', '7. Outer Background Radius in px (eg. 20) \n', '8. Channel# (1,2,3 or 4) \n', '9. Aperture Correction Factor (collect from iracinstrumenthandbook/27, e.g. 1.000) \n', '10. Length of a pixel in arcsec (e.g. 1.221 for ch1) \n', '11. Run# (For output file name. Should be an integer)\n', '12. Sigma Clipping Number (e.g. 10) \n', '13. Comments (to be included in the log) \n', 'Don\'t jump to any argument. e.g. You can\'t skip sigma to get to comments. comments must be the 14th argument. \n', 'You could however, use \'n/a\' for sigma if you don\'t want sigma clipping \n', 'Example command: single hms;/data1/phot_cal/spitzer/hd165459/cryo/r11638016,18 02 30.7410086899 +58 37 38.157415821;bcd;HD 165459;10;12;20;1;1.0;1.221;14;10;Your comment goes here. \n', '\n'
 
     const_str = raw_input('Input the parameters listed above: ') 
     constants = const_str.split(';')
     print constants
 
     crdFormat    = constants[0] 
-    aor_crd      = constants[1] if 'csv' in constants[0] else constants[1].split(',') 
+    aor_crd      = constants[1] if 'csv' in constants[1] else constants[1].split(',') 
     filetype     = constants[2].lower()
     r, rIn, rOut = int(constants[4]), int(constants[5]), int(constants[6])
     channel      = int(constants[7])
@@ -173,7 +180,7 @@ if __name__=='__main__':
 
     #----------------------------
     
-    
+    pdb.set_trace()
     #Generating & writing data tables to csv files
     res, data, prob = run(crdFormat, aor_crd, channel, filetype, r, rIn, rOut, ap_corr, pixLen, N)
     ascii.write(res, 'Reduction_Data_&_Logs/run%s_aor_data.csv' % constants[10], delimiter = ',', overwrite = True)
@@ -191,7 +198,7 @@ if __name__=='__main__':
     log.write("Target       : %s \n" % constants[3])
     log.write("Radius Used  : src_r-%i, bkg_in-%i, bkg_out-%i \n" % (r, rIn, rOut))
     log.write("Average Flux : %.2f +- %.2f \n" % (np.mean(res['Flux (mJy)']), np.std(res['Flux (mJy)'])))
-    log.write("Problem AORs : " + ("%i "*len(prob) % tuple(prob)) + "\n")
+    log.write("Problem AORs : " + ("%s "*len(prob) % tuple(prob)) + "\n")
     log.write("Comments     : %s" % constants[12])
     log.close()
     #----------------------------------------------------------
