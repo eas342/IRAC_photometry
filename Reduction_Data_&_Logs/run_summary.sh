a6de0978d8850bc0edf2df551d2655c0c3c29b55
Rafia Bushra <rafia0037@gmail.com>
2018-12-11 14:12:29 -0700

    new run_summary that lists everything but problem aors

---
+++ Reduction_Data_&_Logs/run_summary.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+
+echo "-------------"
+echo "Date Reduced"
+echo "-------------"
+echo
+grep "Date Reduced" *.txt
+echo
+
+echo "------------------"
+echo "Input Parameters"
+echo "------------------"
+echo
+grep "Input Parameters" *.txt
+echo
+
+echo "-----------"
+echo "Instrument"
+echo "-----------"
+echo
+grep "Instrument" *.txt
+echo
+
+echo "----------"
+echo "File Type"
+echo "----------"
+echo
+grep "File Type" *.txt
+echo
+
+echo "-------"
+echo "Target"
+echo "-------"
+echo
+grep "Target" *.txt
+echo
+
+echo "------------"
+echo "Radius Used"
+echo "------------"
+echo
+grep "Radius Used" *.txt
+echo
+
+echo "---------"
+echo "Comments"
+echo "---------"
+echo
+grep "Radius Used" *.txt
+echo
